2c614de1096660bbafd9db943ef014d2d7fe3ed2
Kat-99 <kat.lucina@gmail.com>
2019-12-17 09:51:32 -0400

    Sort By

--- src/Controller/BillyController.php
+++ src/Controller/BillyController.php
@@ -4,22 +4,29 @@ namespace App\Controller;
 
 use App\Entity\Files;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BillyController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param Request $request
+     * @return RedirectResponse|Response
      */
-    public function index()
+    public function index(Request $request)
     {
 
         if(!$this->getUser()) {
             return $this->redirectToRoute('user_register');
         }
 
+        $order = $request->query->get('sort') ?? 'id';
+
         $filesRepository = $this->getDoctrine()->getRepository(Files::class);
-        $files = $this->getUser() ? $filesRepository->findByUser($this->getUser()) : [];
+        $files = $this->getUser() ? $filesRepository->findBy(['user' => $this->getUser()], [ $order => 'DESC' ]) : [];
 
         return $this->render('home/home.html.twig', [
             'files' => array_reverse($files)
